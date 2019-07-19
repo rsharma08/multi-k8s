163ff2afb9b13d7867032963bdceb60201dd5268
Raman Sharma <raman.sharma@msn.com>
2019-07-19 11:52:29 +0100

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-docker build -t rsharma/muti-client:latest -t rsharma/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t rsharma/muti-server:latest -t rsharma/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t rsharma/muti-worker:latest -t rsharma/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push rsharma/muti-client:latest
-docker push rsharma/muti-server:latest
-docker push rsharma/muti-worker:latest
-docker push rsharma/muti-client:$SHA
-docker push rsharma/muti-server:$SHA
-docker push rsharma/muti-worker:$SHA
+docker build -t rsharma/multi-client:latest -t rsharma/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rsharma/multi-server:latest -t rsharma/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rsharma/multi-worker:latest -t rsharma/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push rsharma/multi-client:latest
+docker push rsharma/multi-server:latest
+docker push rsharma/multi-worker:latest
+
+docker push rsharma/multi-client:$SHA
+docker push rsharma/multi-server:$SHA
+docker push rsharma/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server = rsharma/muti-server:$SHA
-kubectl set image deployments/client-deployment client = rsharma/muti-client:$SHA
-kubectl set image deployments/worker-deployment worker = rsharma/muti-worker:$SHA
+kubectl set image deployments/server-deployment server=rsharma/multi-server:$SHA
+kubectl set image deployments/client-deployment client=rsharma/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=rsharma/multi-worker:$SHA
